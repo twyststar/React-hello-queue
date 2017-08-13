7f8c3995bf4b1fd21ab0078c2bb8194fb1f839a5
Starlynne <lewis.start0905@gmail.com>
2017-08-12 21:01:20 -0700

    Create basic react app that displays live time

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function liveTime() {
+
+  var greeting = React.createElement('h1', {}, 'Hello, React!');
+
+  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+
+  var app = React.createElement('div', {}, greeting, clock);
+
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+
+}
+
+setInterval(liveTime, 1000);
